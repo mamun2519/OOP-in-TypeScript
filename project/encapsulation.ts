dc32475840db8e86baa607d46202d68c3eb4bb90
mamun2519 <juborajislam46@gmail.com>
2025-01-27 19:12:12 +0600

    Practice: Practice OOP concept

---
+++ project/encapsulation.ts
@@ -0,0 +1,8 @@
+class TemperatureController {
+  private currentTemp: number;
+  private maxTemp: number;
+  constructor(currentTemp: number, maxTemp: number) {
+    this.currentTemp = currentTemp;
+    this.maxTemp = maxTemp;
+  }
+}
