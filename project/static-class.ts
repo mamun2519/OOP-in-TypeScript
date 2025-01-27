9b61e2fd48bcb8f896a23bc86afd701f522a18da
mamun2519 <juborajislam46@gmail.com>
2025-01-27 19:32:23 +0600

    Practice: Practice OOP concept

--- project/static-class.ts
+++ project/static-class.ts
@@ -1,4 +1,17 @@
 class AppConfig {
   private static instance: AppConfig;
   private constructor(public apiURL: string) {}
+  static getInstance(): AppConfig {
+    if (!AppConfig.instance) {
+      AppConfig.instance = new AppConfig("https://api.example.com");
+    }
+    return AppConfig.instance;
+  }
+
+  // Static utility
+  static isProduction(): boolean {
+    return process.env.NODE_ENV === "production";
+  }
 }
+
+export const config = AppConfig.getInstance();
