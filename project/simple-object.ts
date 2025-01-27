d85597b6b59a3e29bca70e13a773f5d163476aab
mamun2519 <juborajislam46@gmail.com>
2025-01-27 18:39:51 +0600

    Practice: Practice OOP concept

--- project/simple-object.ts
+++ project/simple-object.ts
@@ -2,5 +2,13 @@ class UserProfile {
   name: string;
   email: string;
   password: string;
-  constructor(name: string, email: string, password: string) {}
+  constructor(name: string, email: string, password: string) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  getFullAddress() {
+    return `Requested User Name ${this.name} and his email ${this.email}`;
+  }
 }
