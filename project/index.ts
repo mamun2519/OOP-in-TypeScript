9cf4baab6bf587424b675484c9783c8fc4aadd8f
mamun2519 <juborajislam46@gmail.com>
2025-01-27 19:33:01 +0600

    Practice: Practice OOP concept

--- project/index.ts
+++ project/index.ts
@@ -1,7 +1,9 @@
 import { user, studentInfo } from "./simple-object";
 import { reactor } from "./encapsulation";
+import { config } from "./static-class";
 // console.log(user);
 // console.log(user.getFullAddress());
 
 // console.log(studentInfo);
-console.log(reactor);
+// console.log(reactor);
+console.log(config);
