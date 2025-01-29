a738799cebb1b40fe91a0f1e5f5b4618a3381aff
mamun2519 <juborajislam46@gmail.com>
2025-01-29 21:41:19 +0600

    Basic OOP pratice

--- project/oopProject.ts
+++ project/oopProject.ts
@@ -1,16 +1,6 @@
 class StripePayment {
   private stripeSecretKey = demoAppConfig?.stripe?.serverSecretKey;
 
-  private Stripe() {
-    return new Stripe(this.stripeSecretKey);
-  }
-  public async paymentInit({ customerInfo, amount }: TPaymentInit) {
-    const { name, email } = customerInfo;
-    if (!email || !amount) {
-      throw new Error("email or amount is required");
-    }
-    const amountInCents = parseInt(amount, 10) * 100;
-    if (isNaN(amountInCents) || amountInCents <= 0) {
-    }
+
   }
 }
