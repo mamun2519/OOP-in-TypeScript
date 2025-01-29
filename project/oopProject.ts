bfae6c23411055d487a0fcef25bd18e4970b8c8d
mamun2519 <juborajislam46@gmail.com>
2025-01-29 19:36:36 +0600

    Basic OOP pratice

--- project/oopProject.ts
+++ project/oopProject.ts
@@ -6,10 +6,41 @@ class StripePayment {
   }
   public async paymentByStripe({ customerInfo, amount }: TPaymentInit) {
     const { name, email } = customerInfo;
-
+    if (!email || !amount) {
+      throw new Error("email or amount is required");
+    }
     const amountInCents = parseInt(amount, 10) * 100;
+    if (isNaN(amountInCents) || amountInCents <= 0) {
+      throw new Error("Invalid amount value");
+    }
 
     try {
-    } catch (error) {}
+      const customer = await this.createCustomer({
+        name,
+        email,
+      });
+
+      const paymentIntent = await this.createPaymentIntents({
+        amount,
+        email,
+        customerId: customer.id,
+      });
+      return {
+        clientSecret: paymentIntent.client_secret,
+      };
+    } catch (error) {
+      if (error instanceof Stripe.errors.StripeError) {
+        switch (error.type) {
+          case "StripeCardError":
+            throw new Error(`A payment error occurred: ${error.message}`);
+          case "StripeInvalidRequestError":
+            throw new Error(`Invalid request: ${error.message}`);
+          case "StripeAPIError":
+            throw new Error(`Internal Stripe API error: ${error.message}`);
+          case "StripeConnectionError":
+            throw new Error(`Network error: ${error.message}`);
+        }
+      }
+    }
   }
 }
