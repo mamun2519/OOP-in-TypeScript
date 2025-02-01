c96dd1f5afa08af702e02ef6721621747721d725
mamun2519 <juborajislam46@gmail.com>
2025-02-01 17:08:36 +0600

    Practice inhertance concept

--- project/inhertance/Ebook.ts
+++ project/inhertance/Ebook.ts
@@ -14,4 +14,16 @@ export class Ebook extends Product {
   getDescription(): string {
     return `${this.title} by ${this.author} | Download Link: ${this._downloadLink} | Total Downloads: ${this.totalDownloads}`;
   }
+
+  set price(price: number) {
+    throw new Error("Cannot set price for Ebook");
+  }
+
+  download() {
+    console.log("Download Ebook");
+  }
+
+  checkStock(_quantity: number): string {
+    return "Available for download";
+  }
 }
