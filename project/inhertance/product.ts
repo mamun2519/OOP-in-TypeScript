ef66518684ed94686cb32ef23e29e0563e64e08e
mamun2519 <juborajislam46@gmail.com>
2025-02-01 16:10:19 +0600

    Practice inhertance concept

--- project/inhertance/product.ts
+++ project/inhertance/product.ts
@@ -1,3 +1,13 @@
-class Product = {
-      
+class Product {
+  protected _discount: number = 0;
+  public readonly id: string;
+  public readonly title: string;
+  protected _price: number;
+  private _stock: number;
+  constructor(id: string, title: string, _price: number, _stock: number) {
+    this.id = id;
+    this.title = title;
+    this._price = _price;
+    this._stock = _stock;
+  }
 }
