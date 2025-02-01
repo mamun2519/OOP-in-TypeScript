8e9ee0a7b79f051f97de982731e6224f68508991
mamun2519 <juborajislam46@gmail.com>
2025-02-01 17:21:56 +0600

    Practice inhertance concept

--- project/inhertance/Magazine.ts
+++ project/inhertance/Magazine.ts
@@ -1,3 +1,28 @@
 import { Product } from "./product";
 
-export class Magazine extends Product {}
+export class Magazine extends Product {
+  constructor(
+    public id: string,
+    public title: string,
+    protected _price: number,
+    protected _stock: number,
+    public issueNumber: number,
+    public publisher: string,
+    public releaseDate: Date
+  ) {
+    super(id, title, _price, _stock);
+  }
+  override getDescription(): string {
+    return `${this.title} | Issue: ${this.issueNumber} | Publisher: ${this.publisher} | Release Date: ${this.releaseDate} | Price: $${this.price} | Stock: ${this.stock}\n`;
+  }
+  override applyDiscount(discount: number): void {
+    super.applyDiscount(discount + 0.05);
+  }
+  checkStock(quantity: number): string {
+    return this._stock >= quantity ? "In Stock" : "Out of Stock";
+  }
+
+  rent() {
+    console.log("Rent Magazine");
+  }
+}
--- project/inhertance/audioBook.ts
+++ project/inhertance/audioBook.ts
@@ -1,18 +1,3 @@
 import { Product } from "./product";
 
-export class AudionBook extends Product {
-  constructor(
-    public id: string,
-    public title: string,
-    protected _price: number,
-    protected _stock: number,
-    public issueNumber: number,
-    public publisher: string,
-    public releaseDate: Date
-  ) {
-    super(id, title, _price, _stock);
-  }
-  override getDescription(): string {
-    return `${this.title} | Issue: ${this.issueNumber} | Publisher: ${this.publisher} | Release Date: ${this.releaseDate} | Price: $${this.price} | Stock: ${this.stock}\n`;
-  }
-}
+export class AudionBook extends Product {}
