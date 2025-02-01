dc8aed31fac5bc90a8b877f7d6138d9b62ee52c1
mamun2519 <juborajislam46@gmail.com>
2025-02-01 17:08:16 +0600

    Practice inhertance concept

--- project/inhertance/Ebook.ts
+++ project/inhertance/Ebook.ts
@@ -1,3 +1,17 @@
 import { Product } from "./product";
 
-export class Ebook extends Product {}
+export class Ebook extends Product {
+  private totalDownload: number;
+  constructor(
+    public id: string,
+    title: string,
+    public author: string,
+    protected _price: number,
+    private _totalDownload: number
+  ) {
+    super(id, title, _price, 1);
+  }
+  getDescription(): string {
+    return `${this.title} by ${this.author} | Download Link: ${this._downloadLink} | Total Downloads: ${this.totalDownloads}`;
+  }
+}
