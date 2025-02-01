d44d064bb0d140e020307361b68c427681f5829f
mamun2519 <juborajislam46@gmail.com>
2025-02-01 16:17:16 +0600

    Practice inhertance concept

--- project/index.ts
+++ project/index.ts
@@ -1,9 +1,14 @@
 import { user, studentInfo } from "./simple-object";
 import { reactor } from "./encapsulation";
 import { config } from "./static-class";
+import { Product } from "./inhertance/product";
+
 // console.log(user);
 // console.log(user.getFullAddress());
 
 // console.log(studentInfo);
 // console.log(reactor);
-console.log(config);
+// console.log(config);
+
+const product = new Product();
+console.log(Product);
